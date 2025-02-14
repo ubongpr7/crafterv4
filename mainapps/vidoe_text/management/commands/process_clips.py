8d3fbc054d3d89c20c99b23f2df270d359271e7d
UbongPr7 <ubongpr7@gmail.com>
2025-02-14 07:37:29 +0000

    tiktok video

--- mainapps/vidoe_text/management/commands/process_clips.py
+++ mainapps/vidoe_text/management/commands/process_clips.py
@@ -1937,7 +1937,7 @@ class Command(BaseCommand):
             
             box_width = min(text_width + small_margin, clip.w * 0.9)
             if self.text_file_instance.resolution =='9:16':
-                box_width=min(text_width + small_margin, clip.w * 0.82)
+                box_width=max(text_width + small_margin, clip.w * 0.82)
 
             box_height = text_height + margin
             rounded_box_array = self.create_rounded_rectangle((int(box_width), int(box_height)), int(box_radius))
